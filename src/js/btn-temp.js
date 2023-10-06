4045557838fbea97033196d0bd94e1dcd625aa84
ZbyszekZebrowski <137501903+ZbyszekZebrowski@users.noreply.github.com>
2023-10-06 17:29:11 +0300

    fix problems with load more

--- src/js/btn-temp.js
+++ src/js/btn-temp.js
@@ -5,10 +5,9 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
-    formEL: document.querySelector('#form'),
-    galleryListEl: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('.load-more'),
-    
+  formEL: document.querySelector('#form'),
+  galleryListEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 }
 
 const simple = new SimpleLightbox('.gallery a');
@@ -18,67 +17,66 @@ refs.formEL.addEventListener('submit', onFormElSubmit);
 refs.loadMoreBtn.addEventListener('click', onloadMoreBtnClick);
 
 async function onFormElSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    pixabayApi.q = event.target.elements.searchQuery.value.trim();
-    pixabayApi.page = 1;
+  pixabayApi.q = event.target.elements.searchQuery.value.trim();
+  pixabayApi.page = 1;
 
-    if (pixabayApi.q === '') {
-        Notiflix.Notify.failure('Enter a word to search for');
-        refs.loadMoreBtn.classList.add('is-hidden');
-        return;
-    } 
+  if (pixabayApi.q === '') {
+    Notiflix.Notify.failure('Enter a word to search for');
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
 
-    try {
-    
-        const data = await pixabayApi.getGalleryCard();
-        
+  try {
+    const data = await pixabayApi.getGalleryCard();
+
     if (data.total === 0) {
-         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        refs.loadMoreBtn.classList.add('is-hidden');
-        event.target.reset();
-
-        refs.galleryListEl.innerHTML = '';
-    
-        return;
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      refs.loadMoreBtn.classList.add('is-hidden');
+      event.target.reset();
+      refs.galleryListEl.innerHTML = '';
+      return;
     }
 
     if (pixabayApi.page === 1) {
-        refs.galleryListEl.innerHTML = galleryTemplate(data.hits);
-        simple.refresh();
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)       
-        refs.loadMoreBtn.classList.remove('is-hidden');                  
-    } 
-    } catch (err) {
-        console.log(`Error: ${err}`)
+      if (data.hits.length < 40) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+      }
+      refs.galleryListEl.innerHTML = galleryTemplate(data.hits);
+      simple.refresh();
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      refs.loadMoreBtn.classList.remove('is-hidden');
     }
-
-
-}
- 
- async function onloadMoreBtnClick(event) {
-    pixabayApi.page += 1;
-    try {
-        const data = await pixabayApi.getGalleryCard();
-        refs.galleryListEl.insertAdjacentHTML('beforeend', galleryTemplate(data.hits));
-        simple.refresh();
-        refs.loadMoreBtn.classList.remove('is-hidden');
-        const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-
-        window.scrollBy({
-            top: cardHeight * 2,
-            behavior: "smooth",
-        });
-
-
-        const lastPage = Math.floor(data.totalHits / pixabayApi.per_page)
-        
-        if (pixabayApi.page === lastPage ) {
-            refs.loadMoreBtn.classList.add('is-hidden');
-            Notiflix.Notify.info('Sorry, but you have reached the end of the search results');
-        }
-    } catch (err) {
-        console.log(`Error: ${err}`)
-    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 }
 
+async function onloadMoreBtnClick(event) {
+  pixabayApi.page += 1;
+  try {
+    const data = await pixabayApi.getGalleryCard();
+    refs.galleryListEl.insertAdjacentHTML('beforeend', galleryTemplate(data.hits));
+    simple.refresh();
+    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
+
+    const lastPage = Math.ceil(data.totalHits / pixabayApi.per_page);
+
+    if (pixabayApi.page === lastPage) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      Notiflix.Notify.info('Sorry, but you have reached the end of the search results');
+    } else if (data.hits.length < 40) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+    } else {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+}
